55303705578704769f13cae17581adbde82abf66
Downtime <40173605+YRainbbbb@users.noreply.github.com>
2020-12-23 22:29:22 +0800

    use bevy rapier
    TODO: We need a smart system to help us when player wants to move,
    // the system will fix step size and related Animation.

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,37 @@
 use bevy::prelude::*;
+use bevy_rapier2d::rapier::{
+    dynamics::RigidBodyBuilder,
+    geometry::ColliderBuilder,
+};
+use bevy_rapier2d::render::RapierRenderPlugin;
+use bevy_rapier2d::{
+    na::{Vector, Vector2},
+    physics::{RapierConfiguration, RapierPhysicsPlugin, RigidBodyHandleComponent},
+    rapier::dynamics::RigidBodySet,
+};
 
-// TODO: should be one represent one tile
-#[derive(Debug)]
-struct Position {
-    x: i32,
-    y: i32,
+fn main() {
+    App::build()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(RapierPhysicsPlugin)
+        .add_plugin(RapierRenderPlugin)
+        .add_startup_system(setup.system())
+        .add_startup_system(physical_setup.system())
+        .add_resource(Map::first())
+        .add_startup_stage(GMAE_SETUP, SystemStage::parallel()) // <--
+        .add_startup_system_to_stage(GMAE_SETUP, game_setup_room.system())
+        .add_stage(MOVEMENT, SystemStage::parallel())
+        .add_system_to_stage(MOVEMENT, change_direction.system())
+        .add_system_to_stage(MOVEMENT, player_movement.system())
+        .run();
 }
-
-impl Position {
-    pub fn set(&mut self, x: i32, y: i32) {
-        self.x = x;
-        self.y = y;
-    }
-    pub fn set_x(&mut self, x: i32) {
-        self.x = x;
-    }
-    pub fn set_y(&mut self, y: i32) {
-        self.y = y;
-    }
+fn physical_setup(mut config: ResMut<RapierConfiguration>) {
+    config.gravity = Vector::y() * 0.0;
 }
-
+// TODO: We need a smart system to help us when player wants to move,
+// the system will fix step size and related Animation.
 const TILE_WIDTH: i32 = 20;
-// TODD:should be removed this, it prevents scalability
-const SIDE_TILE_COUNT: i32 = 10;
-
+const HALF_TITLE_WIDTH: i32 = 10;
 struct Wall;
 
 struct Destructable;
@@ -94,10 +102,23 @@ fn game_setup_room(
                             ..Default::default()
                         })
                         .with(Wall)
-                        .with(Position {
-                            x: col_index as i32,
-                            y: (room_map.len() - row_index - 1) as i32,
-                        });
+                        .with(
+                            RigidBodyBuilder::new_static()
+                                .translation(
+                                    col_index as f32 * TILE_WIDTH as f32,
+                                    (room_map.len() - row_index - 1) as f32 * TILE_WIDTH as f32,
+                                )
+                                .lock_rotations()
+                                .lock_translations(),
+                        )
+                        .with(
+                            ColliderBuilder::cuboid(
+                                HALF_TITLE_WIDTH as f32,
+                                HALF_TITLE_WIDTH as f32,
+                            )
+                            .friction(0.0)
+                            .restitution(0.0),
+                        );
                 }
 
                 2 => {
@@ -108,10 +129,23 @@ fn game_setup_room(
                             ..Default::default()
                         })
                         .with(Wall)
-                        .with(Position {
-                            x: col_index as i32,
-                            y: (room_map.len() - row_index - 1) as i32,
-                        });
+                        .with(
+                            RigidBodyBuilder::new_static()
+                                .translation(
+                                    col_index as f32 * TILE_WIDTH as f32,
+                                    (room_map.len() - row_index - 1) as f32 * TILE_WIDTH as f32,
+                                )
+                                .lock_rotations()
+                                .lock_translations(),
+                        )
+                        .with(
+                            ColliderBuilder::cuboid(
+                                HALF_TITLE_WIDTH as f32,
+                                HALF_TITLE_WIDTH as f32,
+                            )
+                            .friction(0.0)
+                            .restitution(0.0),
+                        );
                 }
                 // When setting each level, the player’s position should be set flexibly
                 3 => {
@@ -123,37 +157,31 @@ fn game_setup_room(
                         })
                         .with(Player { is_moving: false })
                         .with(Direction::Right)
-                        .with(Position {
-                            x: col_index as i32,
-                            y: (room_map.len() - row_index - 1) as i32,
-                        })
-                        .with(Velocity::new(0.1));
+                        .with(Velocity::new(5.0))
+                        .with(
+                            RigidBodyBuilder::new_dynamic()
+                                .translation(
+                                    col_index as f32 * TILE_WIDTH as f32,
+                                    (room_map.len() - row_index - 1) as f32 * TILE_WIDTH as f32,
+                                )
+                                .lock_rotations(),
+                        )
+                        .with(
+                            ColliderBuilder::cuboid(
+                                HALF_TITLE_WIDTH as f32,
+                                HALF_TITLE_WIDTH as f32,
+                            )
+                            .friction(0.0)
+                            .restitution(0.0),
+                        );
                 }
                 _ => continue,
             }
         }
     }
-
-    // for (_player, mut transform) in &mut player_position.iter_mut() {
-    //     transform.translation += Vec3::new(10.0, 0.0, 0.0);
-    // }
 }
-//  be related to map generate,should use resource to stored this
-// const ROOM_WIDTH:u32 = TILE_WIDTH * 11;
-// const ROOM_HEIGHT:u32 = TILE_WIDTH * 11;
 
-fn position_translation(mut q: Query<(&Position, &mut Transform)>) {
-    for (pos, mut transform) in q.iter_mut() {
-        transform.translation = Vec3::new(
-            (TILE_WIDTH * pos.x) as f32,
-            (TILE_WIDTH * pos.y) as f32,
-            0.0,
-        );
-    }
-}
 struct Map {
-    height: i32,
-    width: i32,
     value: Vec<Vec<i32>>,
 }
 impl Map {
@@ -163,42 +191,19 @@ impl Map {
             vec![1, 0, 2, 2, 0, 0, 0, 0, 2, 0, 1],
             vec![1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
             vec![1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            vec![1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+            vec![1, 3, 1, 0, 1, 0, 1, 0, 1, 0, 1],
             vec![1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
             vec![1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
             vec![1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            vec![1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-            vec![1, 3, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            vec![1, 0, 2, 0, 1, 0, 1, 0, 1, 0, 1],
+            vec![1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
             vec![1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
         ];
-        Self {
-            height: 11,
-            width: 11,
-            value: room_map,
-        }
+        Self { value: room_map }
     }
 
     pub fn map_value(&self) -> &Vec<Vec<i32>> {
         &self.value
-    }
-    pub fn height(&self) -> i32 {
-        self.height
-    }
-    pub fn width(&self) -> i32 {
-        self.width
-    }
-    pub fn intersects(&self, x: i32, y: i32) -> bool {
-        // x: (col_index as u32) as i32,
-        // y: ((room_map.len() - row_index - 1) as u32) as i32,
-        let len = self.value.len();
-        if let Some(row) = self.value.get(len - y as usize - 1) {
-            if let Some(num) = row.get(x as usize) {
-                if *num == 0 || *num == 3 {
-                    return true;
-                }
-            }
-        }
-        false
     }
 }
 // ok
@@ -208,56 +213,50 @@ fn change_direction(
 ) {
     for (mut direction, mut player) in query.iter_mut() {
         let movement_action = if keyboard_input.pressed(KeyCode::Left) {
-            //println!("left");
             Some(Direction::Left)
         } else if keyboard_input.pressed(KeyCode::Down) {
-            // println!("down");
             Some(Direction::Down)
         } else if keyboard_input.pressed(KeyCode::Up) {
-            //println!("up");
             Some(Direction::Up)
         } else if keyboard_input.pressed(KeyCode::Right) {
-            //println!("right");
             Some(Direction::Right)
         } else {
-            //println!("none");
             None
         };
         if let Some(dir) = movement_action {
             *direction = dir;
             player.is_moving = true;
-            //println!("moving!");
         } else {
             player.is_moving = false;
-            //println!("stop!");
         }
     }
 }
 
 fn player_movement(
-    map_resource: Res<Map>,
     time: Res<Time>,
-    mut query: Query<(&mut Velocity, &Player, &Direction, &mut Position), (Changed<Player>)>,
+    mut rigid_body_handle_query: Query<&mut RigidBodyHandleComponent>,
+    mut rigid_body_set: ResMut<RigidBodySet>,
+    mut query: Query<(Entity, &Velocity, &Player, &Direction), (Changed<Player>)>,
 ) {
-    for (mut velocity, player, direction, mut position) in query.iter_mut() {
+    for (entity, velocity, player, direction) in query.iter_mut() {
+        let rigid_body_handle = rigid_body_handle_query
+            .get_component_mut::<RigidBodyHandleComponent>(entity)
+            .unwrap();
+
+        let rigid_body = rigid_body_set.get_mut(rigid_body_handle.handle()).unwrap();
+
+        let mut x = 0.0;
+        let mut y = 0.0;
         if player.is_moving {
-            let mut x = position.x;
-            let mut y = position.y;
-
             match direction {
-                Direction::Left => x -= 1,
-                Direction::Up => y += 1,
-                Direction::Right => x += 1,
-                Direction::Down => y -= 1,
-            }
-
-            // println!("{:?}",map_resource.intersects(x, y));
-            // println!("{:?}", velocity.finished(time.delta_seconds()));
-            if map_resource.intersects(x, y) && velocity.finished(time.delta_seconds()) {
-                println!("here!");
-                position.set(x, y);
+                Direction::Left => x -= velocity.value * time.delta_seconds() * 500.0,
+                Direction::Up => y += velocity.value * time.delta_seconds() * 500.0,
+                Direction::Right => x += velocity.value * time.delta_seconds() * 500.0,
+                Direction::Down => y -= velocity.value * time.delta_seconds() * 500.0,
             }
         }
+        // println!("here! x:{},y:{}", x, y);
+        rigid_body.set_linvel(Vector2::new(x, y), true);
     }
 }
 
@@ -265,33 +264,14 @@ const GMAE_SETUP: &str = "game_setup";
 const MOVEMENT: &str = "movement";
 
 struct Velocity {
-    timer: Timer,
+    value: f32,
 }
 
 impl Velocity {
     pub fn new(value: f32) -> Self {
-        let timer = Timer::from_seconds(value, true);
-        Self { timer }
+        Self { value }
     }
     pub fn set(&mut self, value: f32) {
-        self.timer.set_duration(value);
+        self.value = value;
     }
-    pub fn finished(&mut self,delta: f32) -> bool {
-        // TODO: here will be replaced by a better way
-        self.timer.tick(delta).just_finished()
-    }
-}
-
-fn main() {
-    App::build()
-        .add_plugins(DefaultPlugins)
-        .add_startup_system(setup.system())
-        .add_resource(Map::first())
-        .add_startup_stage(GMAE_SETUP, SystemStage::parallel()) // <--
-        .add_startup_system_to_stage(GMAE_SETUP, game_setup_room.system())
-        .add_stage(MOVEMENT, SystemStage::serial())
-        .add_system_to_stage(MOVEMENT, change_direction.system())
-        .add_system_to_stage(MOVEMENT, player_movement.system())
-        .add_system(position_translation.system())
-        .run();
 }
